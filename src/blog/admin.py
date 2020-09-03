33ebda6fa41dd9d4c792f0dce5497208ab424d2e
sopham <shuong1012@gmail.com>
2020-09-03 09:25:49 +0300

    change blog and organizer admin registeration

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from blog.models import Post
-from organizer.models import Tag, Startup, NewsLink
+from .models import Post
 # Register your models here.
 
 admin.site.register(Post)
-admin.site.register(Tag)
-admin.site.register(Startup)
-admin.site.register(NewsLink)
+
--- src/organizer/admin.py
+++ src/organizer/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Tag, Startup, NewsLink
 
 # Register your models here.
+admin.site.register(NewsLink)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+
+@admin.register(Startup)
+class StartupAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
